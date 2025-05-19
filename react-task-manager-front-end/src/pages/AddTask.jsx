99313eb932923f9d7e44cbb3df7f612720d27334
karlo-kasi <karlokasi1995@gmail.com>
2025-05-19 13:14:47 +0200

    Milestone 5 - Creazione del Form per Aggiungere un Task

--- react-task-manager-front-end/src/pages/AddTask.jsx
+++ react-task-manager-front-end/src/pages/AddTask.jsx
@@ -1,5 +1,100 @@
+import { useMemo, useRef, useState } from "react"
+
+const symbols = `!@#$%^&*()-_=+[]{}|;:'\\",.<>?/~`
+
 export default function AddTask() {
+
+    const [isOpenAlert, setisOpenAlert] = useState(false)
+
+    const [title, setTitle] = useState("")
+    const description = useRef()
+    const select = useRef()
+
+    const isTitleValid = useMemo(() => {
+        const charValid = title.split("").some((char) => symbols.includes(char.toLowerCase()))
+        return !charValid
+    }, [title])
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!isTitleValid || !title || !description.current.value || !select.current.value) {
+            return setisOpenAlert(true)
+        } else {
+            console.log({
+                Nome: title,
+                Descrizione: description.current.value,
+                Stato: select.current.value
+            })
+            setisOpenAlert(false)
+        }
+    }
+
     return (
-        <h1>Aggiungi task</h1>
+        <div className="border mt-5 p-4">
+            <h2 className='mb-3 text-center'>COMPILA IL MODULO</h2>
+            <form
+                onSubmit={handleSubmit}
+            >
+                {isOpenAlert && (
+                    <div className="alert alert-danger d-flex align-items-center mt-3" role="alert">
+                        <span className="me-2">⚠️</span>
+                        <div>
+                            Tutti i campi sono obbligatori.
+                        </div>
+                    </div>
+                )}
+                <div className="mb-3">
+                    <label className="form-label">Nome del task</label>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                        className="form-control"
+                        placeholder="Inserisci il tuo nome"
+                    />
+                    {title.trim() && (
+                        <div>
+                            <p className={isTitleValid ? "text-success" : "text-danger"}>
+                                {isTitleValid ? "Username valido" : "Inserisci un Username valido (Non usare Caratteri Speciali)."}
+                            </p>
+                        </div>
+                    )}
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Descrizione</label>
+                    <input
+                        type="text"
+                        ref={description}
+                        className="form-control"
+                        placeholder="Inserisci una Descrizione"
+                    />
+
+                </div>
+
+                <div className="mb-3">
+                    <label className="form-label">Stato</label>
+                    <select
+                        ref={select}
+                        className="form-select"
+                        aria-label="Default select example"
+                        defaultValue="to-do"
+                    >
+                        <option value="to-do">To do</option>
+                        <option value="doing">Doing</option>
+                        <option value="done">Done</option>
+                    </select>
+                </div>
+
+                <div className='d-grid mb-4'>
+                    <button
+                        className='btn btn-primary'
+                        type="submit"
+                    >
+                        AGGIUNGI TASK
+                    </button>
+                </div>
+            </form>
+        </div>
     )
 }
