981501b0cdcd90b84eabc1aabb369f4355f73efd
karlo-kasi <karlokasi1995@gmail.com>
2025-05-21 14:39:12 +0200

    Milestone 11 - Ordinamento delle Task

--- react-task-manager-front-end/src/pages/TaskList.jsx
+++ react-task-manager-front-end/src/pages/TaskList.jsx
@@ -1,9 +1,45 @@
 import { useGlobalContext } from "../context/GlobalContext"
-
+import { useMemo, useState } from "react"
 import TaskRow from "../components/TaskRow"
 
 export default function TaskList() {
     const { tasks } = useGlobalContext()
+
+    const [sortBy, setSortBy] = useState("createdAt")
+    const [sortOrder, setSortOrder] = useState(1)
+
+    const handleSort = (value) => {
+        if (sortBy === value) {
+            setSortOrder(prev => prev * -1)
+        } else {
+            setSortBy(value)
+            setSortOrder(1)
+        }
+    }
+
+
+    const sortedTasks = useMemo(() => {
+        const newArray = [...tasks]
+
+        const statusOrder = {
+            "To do": 0,
+            "Doing": 1,
+            "Done": 2
+        }
+
+        newArray.sort((a, b) => {
+            if (sortBy === "title") {
+                return a.title.localeCompare(b.title) * sortOrder
+            } else if (sortBy === "status") {
+                return (statusOrder[a.status] - statusOrder[b.status]) * sortOrder
+            } else {
+                return (new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()) * sortOrder
+            }
+        })
+
+        return newArray
+
+    }, [tasks, sortBy, sortOrder])
 
     return (
         <div className="mt-4">
@@ -11,12 +47,27 @@ export default function TaskList() {
             <table className="table table-striped table-bordered text-center">
                 <thead className="table-dark">
                     <tr>
-                        <th className="col-6 text-white">Nome</th>
-                        <th className="col-2 text-white">Stato</th>
-                        <th className="col-4 text-white">Data Creazione</th>
+                        <th
+                            className="col-6 text-white"
+                            onClick={() => handleSort("title")}
+                        >
+                            Nome {sortBy === "title" ? (sortOrder === 1 ? "↑" : "↓") : ""}
+                        </th>
+                        <th
+                            className="col-2 text-white"
+                            onClick={() => handleSort("status")}
+                        >
+                            Stato {sortBy === "status" ? (sortOrder === 1 ? "↑" : "↓") : ""}
+                        </th>
+                        <th
+                            className="col-4 text-white"
+                            onClick={() => handleSort("createdAt")}
+                        >
+                            Data Creazione {sortBy === "createdAt" ? (sortOrder === 1 ? "↑" : "↓") : ""}
+                        </th>
                     </tr>
                 </thead>
-                <TaskRow tasks={tasks} />
+                <TaskRow tasks={sortedTasks} />
             </table>
         </div>
 
